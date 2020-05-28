05ebe8d3be04e56df20601bf2ccc735bd0f743d9
Sebastian <jsmontesr@uqvirtual.edu.co>
2020-05-28 18:53:17 -0500

    Generador

--- generador.py
+++ generador.py
@@ -10,15 +10,13 @@ nombreScriptRecursos = "resources.py"
 p = subprocess.Popen ("cat reporte.html", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     #Se espera que acabe el subproceso para mostrar la salida decodificada
 p.wait()
-incial = p.stdout.read().decode().split("//ZonaDeCambio+")[0]
-print(incial)
+inicial = p.stdout.read().decode().split("//ZonaDeCambio+")[0]
 
 #Se cargan los datos
 p = subprocess.Popen ("./" + nombreScriptRecursos, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     #Se espera que acabe el subproceso para mostrar la salida decodificada
 p.wait()
 datos = "//ZonaDeCambio+\n" + p.stdout.read().decode() + "//ZonaDeCambio-"
-print(datos)
 
 #Se cargar la parte final
 p = subprocess.Popen ("cat reporte.html", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -26,7 +24,12 @@ p = subprocess.Popen ("cat reporte.html", stdout=subprocess.PIPE, stderr=subproc
 p.wait()
 final = p.stdout.read().decode().split("//ZonaDeCambio-")[1]
 
-print(final)
+f = open (nombreReporte + ".html", "w")
+archivo = (inicial+datos+final).splitlines(True)
+print(str(archivo))
+for fila in archivo:
+    f.write(fila)
+f.close()
 
 # Se transforma el reporte a pdf y se abre con el xreader
 p = subprocess.Popen ("wkhtmltopdf --javascript-delay 8000 index.html " + nombreReporte + ".pdf && xreader " + nombreReporte, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
